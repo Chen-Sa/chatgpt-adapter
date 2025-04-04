e3b6a2865b27e662fcbc4ba0b03c46cb8bd3165b
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2025-04-05 06:21:23 +0800

    Update fetch.go

--- relay/llm/cursor/fetch.go
+++ relay/llm/cursor/fetch.go
@@ -35,7 +35,7 @@ func fetch(ctx *gin.Context, env *env.Environment, cookie string, buffer []byte)
 	response, err = emit.ClientBuilder(common.HTTPClient).
 		Context(ctx.Request.Context()).
 		Proxies(env.GetString("server.proxied")).
-		POST("http://[2606:4700::6812:127d]/aiserver.v1.AiService/StreamChat").
+		POST("https://[2606:4700::6812:127d]/aiserver.v1.AiService/StreamChat").
 		Header("authorization", "Bearer "+cookie).
 		Header("content-type", "application/connect+proto").
 		Header("connect-accept-encoding", "gzip").
